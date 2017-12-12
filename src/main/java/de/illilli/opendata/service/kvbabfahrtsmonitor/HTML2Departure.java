7bf2930a0df4c80445bb5b9b779dff3e340e107f
Wolfram Eberius <eberius@gmail.com>
2017-12-12 05:43:17 +0100

    Parsing for page has to be changed

--- src/main/java/de/illilli/opendata/service/kvbabfahrtsmonitor/HTML2Departure.java
+++ src/main/java/de/illilli/opendata/service/kvbabfahrtsmonitor/HTML2Departure.java
@@ -24,6 +24,21 @@ public class HTML2Departure {
 	private List<Departure> data = new ArrayList<Departure>();
 	private Date date = new Date();
 
+	/**
+	 * <p>
+	 * To select only td without colspan and without children use
+	 * 'td:not([colspan]):not(:has(*))':
+	 * </p>
+	 * <p>
+	 * <a href=
+	 * "https://stackoverflow.com/questions/9298822/what-would-the-jsoup-selector-be-for-td-elements-that-have-no-children-and-no-co">
+	 * What would the Jsoup selector be for TD elements that have no children
+	 * and no COLSPAN attribute?</a>
+	 * </p>
+	 * 
+	 * @param html
+	 * @throws ParseException
+	 */
 	public HTML2Departure(String html) throws ParseException {
 
 		Document doc = Jsoup.parse(html, "ISO-8859-1");
@@ -31,13 +46,13 @@ public class HTML2Departure {
 
 		Iterator<Element> ite = table.select("td").iterator();
 		while (ite.hasNext()) {
-			String str = ite.next().text();
+			String str = ite.next().text().replace("\u00a0", "").replace("Uhr", "").trim();
 			// '14.04.2017 - 06:58 Uhr'
 			this.date = new SimpleDateFormat("dd.MM.yyyy - hh:mm").parse(str);
 		}
 
 		table = doc.select("table").last();
-		ite = table.select("td").iterator();
+		ite = table.select("td:not([colspan]):not(:has(*))").iterator();
 		for (int i = 0; ite.hasNext(); i++) {
 			Departure departure = new Departure();
 			for (int j = 0; j < 3; j++) {
